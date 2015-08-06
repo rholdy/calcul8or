d4d8014426910d25e1e001736714847a45116c3b
Holdy <rholdy@gmail.com>
2015-08-05 20:52:00 -0600

    Initial Commit

---
+++ Calcul8r/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calcul8r
+//
+//  Created by Ryan Holdaway on 8/5/15.
+//  Copyright (c) 2015 Ryan Holdaway. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
